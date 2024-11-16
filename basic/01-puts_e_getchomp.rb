15a3608b8a8492206a91831be8a98105edea6207
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2024-11-16 10:55:50 -0300

    add desafio puts e gets.chomp dio

---
+++ basic/01-puts_e_getchomp.rb
@@ -0,0 +1,12 @@
+# Receber o nome e idade para um sistema de biblioteca.
+
+puts "Bem vindo ao sistema da biblioteca."
+
+puts "Informe seu nome: "
+nome = gets.chomp
+
+puts "informe sua idade (anos): "
+idade = gets.chomp
+
+puts "#{nome}, #{idade} anos, seja bem-vindo!!!"
+
